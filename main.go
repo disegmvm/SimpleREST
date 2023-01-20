99800580c4822cf2a4194e8b01293cf70483b0ff
denispeganovri <denis.peganov@river-island.com>
2023-01-20 14:30:41 +0100

    Minor refactoring

--- main.go
+++ main.go
@@ -5,19 +5,20 @@ import (
 	"net/http"
 )
 
-// car represents data about each car's record.
-type car struct {
+type Car struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
 	Color string `json:"color"`
 }
 
-var cars = []car{
-	{ID: "1", Title: "BMW", Color: "Black"},
-	{ID: "2", Title: "Tesla", Color: "Red"},
-}
+var Cars []Car
 
 func main() {
+	Cars = []Car{
+		{ID: "1", Title: "BMW", Color: "Black"},
+		{ID: "2", Title: "Tesla", Color: "Red"},
+	}
+
 	router := gin.Default()
 	router.GET("/cars", getCars)
 	router.GET("/car/:id", getCarByID)
@@ -28,54 +29,55 @@ func main() {
 	router.Run("localhost:8080")
 }
 
-func createCar(context *gin.Context) {
-	var newCar car
-	if err := context.BindJSON(&newCar); err != nil {
+func createCar(c *gin.Context) {
+	var newCar Car
+	if err := c.BindJSON(&newCar); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Failed to create a car"})
 		return
 	}
 
-	cars = append(cars, newCar)
-	context.IndentedJSON(http.StatusCreated, newCar)
+	Cars = append(Cars, newCar)
+	c.IndentedJSON(http.StatusCreated, newCar)
 }
 
-func getCars(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, cars)
+func getCars(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Cars)
 }
 
 func getCarByID(c *gin.Context) {
-	for _, car := range cars {
+	for _, car := range Cars {
 		if car.ID == c.Param("id") {
 			c.IndentedJSON(http.StatusOK, car)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": " Requested car is not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Requested car is not found"})
 }
 
 func deleteCar(c *gin.Context) {
-	for index, car := range cars {
+	for index, car := range Cars {
 		if car.ID == c.Param("id") {
-			cars = append(cars[:index], cars[index+1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": " Car is deleted"})
+			Cars = append(Cars[:index], Cars[index+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "Car is deleted"})
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": " Car is not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Car is not found"})
 }
 
 func replaceCar(c *gin.Context) {
-	var newCar car
+	var newCar Car
 	if err := c.BindJSON(&newCar); err != nil {
 		return
 	}
 
-	for index, car := range cars {
+	for index, car := range Cars {
 		if car.ID == c.Param("id") {
-			cars = append(cars[:index], cars[index+1:]...)
-			cars = append(cars, newCar)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": " Car is replaced"})
+			Cars = append(Cars[:index], Cars[index+1:]...)
+			Cars = append(Cars, newCar)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "Car is replaced"})
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": " Car is not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Car is not found"})
 }
